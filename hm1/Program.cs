fed89f03685e71bc78af7af64517049b9b134577
Katerina <katerina3113@bk.ru>
2023-02-06 19:10:45 +0300

    Домашнее задание 1

---
+++ hm1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа через пробел: ");
+int[] a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int sum = 0;
+for (int i=0; i<a.Length; i++)
+{
+    if (a[i]>0)
+    {
+        sum = sum + 1;
+    }
+}
+Console.WriteLine($"{sum} элементов больше нуля");
